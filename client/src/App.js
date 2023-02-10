900f15ea87b0906221d82822336ca052a86d02b9
jullscol <93540150+jullscol@users.noreply.github.com>
2023-02-09 21:54:38 -0500

    zakonchil no nada lomat

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
+import React from 'react';
+import LandingPage from './components/LandingPage/LandingPage.jsx';
+import Home from './components/Home/Home.jsx';
+import ActivityCreate from './components/ActivityCreate/ActivityCreate.jsx';
+import ActivitiesList from './components/ActivitiesList/ActivitiesList.jsx';
+import Detail from './components/Detail/Detail.jsx';
+import NotFound from './components/NotFound/NotFound.jsx';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Countries</h1>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route exact path = '/' component = {LandingPage}/>
+          <Route exact path = '/Home' component = {Home}/>
+          <Route exact path = '/Activity' component = {ActivityCreate}/>
+          <Route exact path = '/Activities' component = {ActivitiesList}/>
+          <Route exact path = '/Home/:id' component = {Detail}/>
+          <Route exact path = '/Home/:*' component = {Detail}/>
+          <Route exact path = '*' component = {NotFound}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
